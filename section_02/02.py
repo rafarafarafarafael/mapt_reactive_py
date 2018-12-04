391373039e1ae2136e84d3f5e4d3164aff21dfcf
Rafael Santos <rafaels@gmail.com>
2018-12-04 02:53:22 -0500

    created custom observables

--- section_02/02.py
+++ section_02/02.py
@@ -4,3 +4,10 @@ def print_value(value):
     print('{} is the value.'.format(value))
 
 Observable.from_(['abc', 'def', 'ghi']).subscribe(print_value)
+
+def say_hello(name, callback):
+    callback('hello {}!'.format(name))
+
+hello = Observable.from_callback(say_hello) # a factory function to create new observables
+hello('Rafael').subscribe(print_value) # actually creates an observable
+hello('observable').subscribe(print_value) # and then this also creates an observable
