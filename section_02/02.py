35e30f639fb89cf813fa138085d78d0e6dbeff30
Rafael Santos <rafaels@gmail.com>
2018-12-04 03:12:41 -0500

    schedule testing streams

--- section_02/02.py
+++ section_02/02.py
@@ -1,4 +1,5 @@
 from rx import Observable
+from rx.testing import marbles, TestScheduler
 
 def print_value(value):
     print('{} is the value.'.format(value))
@@ -14,3 +15,9 @@ hello('observable').subscribe(print_value) # and then this also creates an obser
 
 Observable.from_list([1, 2, 3]).subscribe(print_value)
 Observable.of(1, 2, 3, 'A', 'B', 'C').subscribe(print_value)
+
+test_scheduler = TestScheduler() # used to schedule streams and test the observables like a marble diagram
+Observable.from_marbles('---(a1)-(b2)------(c3)|', test_scheduler).subscribe(print_value)
+Observable.from_marbles('(a6)---(b5)(c4)|', test_scheduler).subscribe(print_value)
+
+test_scheduler.start() # start the scheduling
